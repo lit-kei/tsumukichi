bb611f665884a4b9adf97bf978db14c29ac6a5a1
lit-kei <puyoteto123@yahoo.co.jp>
2025-04-30 21:17:53 +0900

    self.bat was changed

--- self.bat
+++ self.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set /p user_input=Please input the message...:
+set /p user_input=Please input the message:
 
 git add .
-git commit -m %user_input%
+git commit -m "%user_input%"
 git push origin main
 
 echo The process has finished.
